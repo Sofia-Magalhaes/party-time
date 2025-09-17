8898a51218124e6592d50ca432656b43c424b494
Sofia-Magalhaes <52801509+Sofia-Magalhaes@users.noreply.github.com>
2025-09-17 15:34:22 -0300

    Criando página para inserir festas
    modificando também um pouco o backend, colocando novos serviços

---
+++ backend/db/servicesmock.js
@@ -0,0 +1,62 @@
+[
+  {
+    "name": "Pepita",
+    "description": "A melhor cantora que temos, PEPITA!!! Rammm.",
+    "price": 2000,
+    "image": "https://i.pinimg.com/736x/68/64/5f/68645fae490984c9943e95de11f60ffc.jpg"
+  },
+  {
+    "name": "Camila Loures",
+    "description": "Com os melhores hits vem ai ela! A melhor nas publis.",
+    "price": 2500,
+    "image": "https://i.pinimg.com/736x/eb/66/b6/eb66b68e45cb44fa8769313673076dba.jpg"
+  },
+  {
+    "name": "Andressa Urach",
+    "description": "Cabelereira, dona de casa, Imola, evangelica, empresaria, criadora de conteudo adulto, estudande de biomedicina, ela faz tudo e diverte todos .",
+    "price": 1200,
+    "image": "https://i.pinimg.com/736x/39/52/cf/3952cf32407c90c2bdc5d314154e7e32.jpg"
+  },
+  {
+    "name": "Bar da Lora",
+    "description": "Anima a sua festa com muita comida boa, espeto de grelo, buraco fundo, esse são apenas algumas das delicias!",
+    "price": 1800,
+    "image": "https://pt.quizur.com/_image?href=https://img.quizur.com/f/img65326093539378.34949695.jpg?lastEdited=1697800343&w=1024&h=1024&f=webp"
+  },
+  {
+    "name": "Saori Kido",
+    "description": "Com variedades de historias, com toda certeza ela sera a melhor contratação",
+    "price": 2750,
+    "image": "https://i.pinimg.com/736x/3c/b5/80/3cb58025e0e1de100494a4c62abfcab7.jpg"
+  },
+  {
+    "name": "Vanessa Wolf",
+    "description": "Epa epa epa, vamos se divertir?.",
+    "price": 1850,
+    "image": "https://i.pinimg.com/736x/d6/5a/bc/d65abc9a53c8629f32ea949ad8d8a82d.jpg"
+  },
+  {
+    "name": "Patixa",
+    "description": "Cuidado rouba marido",
+    "price": 2100,
+    "image": "https://i.pinimg.com/736x/99/6d/9a/996d9af8f3451fd7e181f7db912d5204.jpg"
+  },
+  {
+    "name": "Vanessa Wolf",
+    "description": "Apenas para servir cunt, diva.",
+    "price": 1400,
+    "image": "https://i.pinimg.com/736x/12/45/9a/12459a6070bd9a9cb48d88ce0a152123.jpg"
+  },
+  {
+    "name": "Selena Gomes",
+    "description": "Hello guys it's me Valeria Almeida",
+    "price": 3000,
+    "image": "https://i.pinimg.com/736x/eb/2c/70/eb2c70da08d81763b4fd4b8466512c77.jpg"
+  },
+  {
+    "name": "Cuppcake",
+    "description": "Um doce de pessoas, músicas mais infantis e mais calmas",
+    "price": 1400,
+    "image": "https://i.pinimg.com/736x/8e/bd/3f/8ebd3f88a31e16be0123eeff557064e4.jpg"
+  }
+]
