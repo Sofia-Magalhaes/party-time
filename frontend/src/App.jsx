053e1c1c78bf88fb347960fd91337bb1494d56b8
Sofia-Magalhaes <52801509+Sofia-Magalhaes@users.noreply.github.com>
2025-09-17 14:42:51 -0300

    Configurando o router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,12 @@
 import './App.css'
 
+import { Outlet } from 'react-router-dom'
+
 function App() {
   return (
     <div className='App'>
       <h1>Party Time</h1>
+      <Outlet />
     </div>
   )
 }
