5adfd877ef9ebad3a0ae8ce2726651eeef521f66
Sofia-Magalhaes <52801509+Sofia-Magalhaes@users.noreply.github.com>
2025-09-17 14:52:41 -0300

    Criação do navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,12 @@ import './App.css'
 
 import { Outlet } from 'react-router-dom'
 
+import Navbar from './components/Navbar'
+
 function App() {
   return (
     <div className='App'>
-      <h1>Party Time</h1>
+      <Navbar />
       <Outlet />
     </div>
   )
