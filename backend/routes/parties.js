a703737a6edc1cf395b0022684df46682fb07c48
Sofia-Magalhaes <52801509+Sofia-Magalhaes@users.noreply.github.com>
2025-09-17 14:28:55 -0300

    Atualizando festas

--- backend/routes/parties.js
+++ backend/routes/parties.js
@@ -2,12 +2,14 @@ const router = require("express").Router()
 
 const partyController = require("../controllers/partyController")
 
-router.route("/parties").post((req, res) => partyController.create(req, res))
+router.route("/parties").post((req, res) => partyController.create(req, res))//criar festa
 
-router.route("/parties").get((req, res) => partyController.getAll(req, res))
+router.route("/parties").get((req, res) => partyController.getAll(req, res))//procurar todas festas
 
-router.route("/parties/:id").get((req, res) => partyController.get(req, res))
+router.route("/parties/:id").get((req, res) => partyController.get(req, res))//procurar festa individual
 
-router.route("/parties/:id").delete((req, res) => partyController.delete(req, res))
+router.route("/parties/:id").delete((req, res) => partyController.delete(req, res))//deletar festa
+
+router.route("/parties/:id").put((req, res) => partyController.update(req, res))//atualizar festa
 
 module.exports = router
